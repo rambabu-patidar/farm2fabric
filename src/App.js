ea2287cf6c662b7161fba2804553d2a440070493
Lanceiz <atuls.90601@gmail.com>
2023-09-18 21:40:51 +0530

    first-commit and file cleaning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-	return <h1 className="text-6xl font-bold underline">Hello Team Avengers!</h1>;
+	return (
+		<h1 className="text-6xl font-bold underline">
+			Hello Team Avengers!
+		</h1>
+	);
 }
 
 export default App;
