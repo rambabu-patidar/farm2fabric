8d6d26834dddc285dd2137d0f7b2d280f822f7f4
Lanceiz <atuls.90601@gmail.com>
2023-09-19 18:41:36 +0530

    landing page edited

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-<<<<<<< HEAD
 import WhyWool from "./pages/WhyWool"
 import FarmersDashboard from "./pages/FarmersDashboard"
 import BuyersDashboard from "./pages/BuyersDashboard"
 import Prices from "./pages/Prices";
-=======
-import WhyWool from "./pages/WhyWool";
-import FarmersDashboard from "./pages/FarmersDashboard";
-import BuyersDashboard from "./pages/BuyersDashboard";
->>>>>>> 84ff55e77dc773a0023e4e8dd2d692939f5d0b02
 
 import { useState } from "react";
 
@@ -24,7 +18,6 @@ function App() {
 		<div>
 			<Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
 			<Routes>
-<<<<<<< HEAD
 				<Route path="/" element= {<Home isLoggedIn={isLoggedIn}/>} />
 				<Route path="/login" element = {<Login  setIsLoggedIn={setIsLoggedIn} />} />
 				<Route path="/signup" element = {<Signup  setIsLoggedIn={setIsLoggedIn} />} />
@@ -32,20 +25,6 @@ function App() {
 				<Route path="/buyersdashboard" element={<BuyersDashboard/>}/>
 				<Route path="/why-wool" element={<WhyWool/>}/>
 				<Route path="/prices" element={<Prices/>}/>
-=======
-				<Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
-				<Route
-					path="/login"
-					element={<Login setIsLoggedIn={setIsLoggedIn} />}
-				/>
-				<Route
-					path="/signup"
-					element={<Signup setIsLoggedIn={setIsLoggedIn} />}
-				/>
-				<Route path="/farmersdashboard" element={<FarmersDashboard />} />
-				<Route path="/buyersdashboard" element={<BuyersDashboard />} />
-				<Route path="/why-wool" element={<WhyWool />} />
->>>>>>> 84ff55e77dc773a0023e4e8dd2d692939f5d0b02
 			</Routes>
 		</div>
 	);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import './Home.css'
+import heroimg from "../assets/logo-igmg.jpg"
+import pic2 from "../assets/pic2.jpg"
+
 
 const Home = () => {
   return (
     <div className='home'>
       {/* page-1(hero section) */}
       <div className='page-1'>
-        <h1 className='heading w-10/12'>From Nature, For Nature</h1>
-        <img className="hero-img" src="" alt="hero img" width={200} height={400} />
+        <div className="heading">
+          <h1 className='hero-title'>From Nature, For Nature</h1>
+          <p>From Farm to Fiber: Uniting Wool Producers and Enthusiasts.
+          Spinning Stories, Weaving Dreams: Your Source for Quality Wool.</p>
+          <button className='btn'>Learn More</button>
+        </div>
+        
+        <img className="hero-img" src={heroimg} alt="hero img" width={200} height={400} />
+      </div>
+
+      {/* page-2 */}
+      <div className="page2">
+        <div className="page2-img">
+          <img src={pic2} alt="pic2" height={10} />
+        </div>
+        <div className="page2-content">
+          <h1>About Us</h1>
+          <p>WoolFarm Connect is a dedicated online platform designed to cater to the needs 
+            of both wool farmers and buyers within the wool industry. Our comprehensive services 
+            bring together the world of wool, making it easier for wool producers and purchasers 
+            to connect, trade, and access valuable resources.
+          </p>
+        </div>
       </div>
     </div>
   )
